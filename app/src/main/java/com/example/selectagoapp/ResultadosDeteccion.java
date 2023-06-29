84f829dcd9a6d06dcea5b3900d2e28b7c3dc8d7e
DiegoWitron234 <maximusdiegoalonsodbz@gmail.com>
2023-06-29 11:05:14 -0600

    Cambios de colores y tema

---
+++ app/src/main/java/com/example/selectagoapp/ResultadosDeteccion.java
@@ -0,0 +1,14 @@
+package com.example.selectagoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResultadosDeteccion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultados_deteccion);
+    }
+}
