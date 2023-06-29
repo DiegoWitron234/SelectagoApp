4d1e350cec79e66dc568ca641585ae86210ccaa6
DiegoWitron234 <maximusdiegoalonsodbz@gmail.com>
2023-06-29 13:09:58 -0600

    Funcionalidad básica al menú principal y agregué algunas activities

--- app/src/main/java/com/example/selectagoapp/MainActivity.kt
+++ app/src/main/java/com/example/selectagoapp/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun fnTomarMuestras(view: View) {
-        val intent = Intent(this, confMuestra::class.java)
+        val intent = Intent(this, ConfMuestra::class.java)
         startActivity(intent)
     }
 
