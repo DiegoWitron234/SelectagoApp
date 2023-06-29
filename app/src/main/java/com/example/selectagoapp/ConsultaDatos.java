e8cf18296d889c022a00037e3768c5d2965755ff
RaulRodriguezRA <RaulAlejandroR2_3@hotmail.com>
2023-06-29 12:38:33 -0600

    Activity confEstimacion v1 Listo

---
+++ app/src/main/java/com/example/selectagoapp/ConsultaDatos.java
@@ -0,0 +1,14 @@
+package com.example.selectagoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConsultaDatos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consulta_datos);
+    }
+}
