fc094347b923a43abbe24e6ea46477c272e82759
RaulRodriguezRA <RaulAlejandroR2_3@hotmail.com>
2023-06-29 11:09:23 -0600

    Activity confMuestra v2

--- app/src/main/java/com/example/selectagoapp/confMuestra.java
+++ app/src/main/java/com/example/selectagoapp/confMuestra.java
@@ -6,29 +6,62 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class confMuestra extends AppCompatActivity {
-    final String[] frutos = new String[]{"Limon", "Mandarina (No Usar)"};
+    final String[] frutos = new String[]{"Tipo de fruto","Limon"};
+    final String[] precision = new String[]{"Nivel de precisión","0.5","0.75","0.95"};
     private Spinner opcionFrutas;
+    private Spinner opcionPrecision;
+    private TextView cantidadArboles;
+    private String nivelPrecision, tipoFruta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conf_muestra);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, frutos);
+        // Instanciando elementos de vista
         opcionFrutas = (Spinner) findViewById(R.id.opcionFrutas);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        opcionFrutas.setAdapter(adapter);
-        eventoFrutaOpcion(opcionFrutas, this);
+        opcionPrecision = (Spinner) findViewById(R.id.opcionPrecision);
+        cantidadArboles = findViewById(R.id.intCantArbol);
+        // Configuración de ArrayAdapters
+        confArrayAdapters(opcionFrutas, opcionPrecision);
+        // Estableciendo evento Listener en los ArrayAdapters
+        eventoOpcionArray(opcionFrutas, opcionPrecision);
   }
 
-    private void eventoFrutaOpcion(Spinner opcionFrutas, confMuestra confMuestra){
+    private void confArrayAdapters(Spinner opcionFrutas, Spinner opcionPrecision){
+        ArrayAdapter<String> adapterFrutas = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, frutos);
+        ArrayAdapter<String> adapterPrecision = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, precision);
+        // ArrayAdapter de Frutas
+        adapterFrutas.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        opcionFrutas.setAdapter(adapterFrutas);
+        // ArrayAdapter de Precision
+        adapterPrecision.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        opcionPrecision.setAdapter(adapterPrecision);
+    }
+
+    private void eventoOpcionArray(Spinner opcionFrutas, Spinner opcionPrecision){
         opcionFrutas.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(confMuestra,"Fruto:"+ frutos[i], Toast.LENGTH_LONG).show();
+                tipoFruta = frutos[i];
+                Toast.makeText(getApplicationContext(),"Fruto: "+ tipoFruta, Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        opcionPrecision.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                nivelPrecision = precision[i];
+                Toast.makeText(getApplicationContext(),"Precisión:"+ nivelPrecision, Toast.LENGTH_LONG).show();
             }
 
             @Override
@@ -36,5 +69,10 @@ public class confMuestra extends AppCompatActivity {
 
             }
         });
+    }
+
+    public void aceptarConfMu(View view) {
+        int numArboles = Integer.parseInt((String) cantidadArboles.getText());
+        Toast.makeText(this, numArboles + nivelPrecision + tipoFruta,Toast.LENGTH_LONG).show();
     }
 }
