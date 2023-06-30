76be22e10653f8d7cfe1be36f974728ee5a15e8b
RaulRodriguezRA <RaulAlejandroR2_3@hotmail.com>
2023-06-30 11:28:31 -0600

    Activity ConsultarDatos V2.1

--- app/src/main/java/com/example/selectagoapp/ConsultaDatos.java
+++ app/src/main/java/com/example/selectagoapp/ConsultaDatos.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
